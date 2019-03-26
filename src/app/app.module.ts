8544d71b82e371ce85e080dd035589e8cc64b4db
Marianne Hval <mariannehval@gmail.com>
2019-03-26 17:06:44 +0100

    Background and images/buttons OK

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
+import { MatButtonModule } from '@angular/material/button';
+
 import { AppComponent } from './app.component';
 import { IntroComponent } from './pages/intro/intro.component';
 import { QuizComponent } from './pages/quiz/quiz.component';
@@ -16,7 +18,8 @@ import { ResultComponent } from './pages/result/result.component';
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
