d3397dd108c195d36998c038f6b042b19f51532a
Marianne Hval <mariannehval@gmail.com>
2019-03-27 17:15:23 +0100

    pokemonstorage.service

--- src/app/pokemonstorage.service.ts
+++ src/app/pokemonstorage.service.ts
@@ -5,5 +5,31 @@ import { Injectable } from '@angular/core';
 })
 export class PokemonstorageService {
 
+  favorites = [];
+
+
   constructor() { }
+
+  savePokemon(category: string, type: string, pokemon) {
+    // Save in local storage.
+    // var testObject = { 'one': 1, 'two': 2, 'three': 3 };
+
+    // // Put the object into storage
+    // localStorage.setItem('testObject', JSON.stringify(testObject));
+
+    // // Retrieve the object from storage
+    // var retrievedObject = localStorage.getItem('testObject');
+
+    // console.log('retrievedObject: ', JSON.parse(retrievedObject));
+
+  }
+}
+
+export class FavoriteCategory {
+  favoriteTypes: FavoriteType[];
+}
+
+export class FavoriteType {
+  type: string;
+  pokemon: any;
 }
