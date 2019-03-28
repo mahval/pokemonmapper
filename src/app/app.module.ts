11c121dd078ff5ca8f17c7cb707ba471d6c9010d
Marianne Hval <mariannehval@gmail.com>
2019-03-28 19:05:10 +0100

    Loading screen + separated pokemonselector screen

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
-import { MatButtonModule, MatFormFieldModule, MatInputModule, MatIconModule, MatSelectModule, MatDividerModule } from '@angular/material';
+import {
+  MatButtonModule, MatFormFieldModule, MatInputModule, MatIconModule,
+  MatSelectModule, MatDividerModule, MatProgressSpinnerModule
+} from '@angular/material';
 
 import { AppComponent } from './app.component';
 import { IntroComponent } from './pages/intro/intro.component';
 import { QuizComponent } from './pages/quiz/quiz.component';
 import { ResultComponent } from './pages/result/result.component';
 import { AppRoutingModule } from './app-routing.module';
+import { PokemonSelectorComponent } from './pages/quiz/pokemon-selector/pokemon-selector.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     IntroComponent,
     QuizComponent,
-    ResultComponent
+    ResultComponent,
+    PokemonSelectorComponent
   ],
   imports: [
     BrowserModule,
@@ -31,7 +36,8 @@ import { AppRoutingModule } from './app-routing.module';
     MatInputModule,
     MatIconModule,
     MatSelectModule,
-    MatDividerModule
+    MatDividerModule,
+    MatProgressSpinnerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
